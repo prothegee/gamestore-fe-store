dc342777cfef9a0c5c85402b9f8895ddd0ca260d
prothegee <prothegee@proton.me>
2026-04-16 11:36:34 +0700

    chore: update dev/start port from 9007 to 9008

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // @ts-@ts-expect-error - allowedDevOrigins is required for local network mobile testing
-  allowedDevOrigins: ['192.168.1.59', 'localhost:9007'],
+  allowedDevOrigins: ['192.168.1.59', 'localhost:9008'],
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
